9f16d46a47f1aca407acf8960cfe8e840971bd95
Thao Phung <ttp7428@truman.edu>
2020-12-23 02:38:37 -0600

    Updates

---
+++ precache-manifest.9f7088ff8c1d836dcadb074412b0d52c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5614c34fb6a44dcc37dc963f11e4f75",
+    "url": "/react-color-guessing-game/index.html"
+  },
+  {
+    "revision": "16d42fc43fea1907c269",
+    "url": "/react-color-guessing-game/static/css/main.77c4753d.chunk.css"
+  },
+  {
+    "revision": "26cd505e95922487fa67",
+    "url": "/react-color-guessing-game/static/js/2.f82e9f68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-color-guessing-game/static/js/2.f82e9f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d42fc43fea1907c269",
+    "url": "/react-color-guessing-game/static/js/main.0817f81d.chunk.js"
+  },
+  {
+    "revision": "70c34ed220e7d702fa4f",
+    "url": "/react-color-guessing-game/static/js/runtime-main.f2667009.js"
+  }
+]);
